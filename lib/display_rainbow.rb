a3a6bf86c467283eecc1600a065ae6f745e09e23
Davonta Trench <trenchdavonta@gmail.com>
2019-10-17 16:58:48 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,3 @@
 def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 prints "R: #{red[0]}, O: #{orange[1]}, Y: #{yellow[2]}, G: #{green[3]}, B: #{blue[4]}, I: #{indigo[5]} V: #{violet[6]}"
-"
 end
